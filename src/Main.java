16ccf294478960d0ea019cf903e2c7a90a60e7f0
GorkyAnge <gorkyange@gmail.com>
2024-04-22 19:38:32 -0500

    Ahora la función de groovy permite variables y realizar acciones con estas

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Shakira shakira = new Shakira();
-
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             System.out.println("Menú:");
             System.out.println("1. Ejecutar Función desde JAVA");
-            System.out.println("2. Ejecutar un script personalizado (utilizando Shakira)");
+            System.out.println("2. Ejecutar un script personalizado");
             System.out.println("3. Salir");
             System.out.print("Ingrese su opción: ");
             int option = scanner.nextInt();
@@ -19,15 +18,14 @@ public class Main {
                     shakira.executeHelloJava();
                     break;
                 case 2:
-
-                    //EJEMPLOS DE SCRIPTS PERSONALIZADOS
-                    //println "Hello from Groovy!"
-
+                    //println "El valor de la variable es: " + variable
+                    //println "El valor de la variable multiplicado por 2 es: " + variable.toInteger() * 2
                     System.out.print("Ingrese el script personalizado: ");
                     scanner.nextLine(); // Consuming newline character
                     String script = scanner.nextLine();
-                    shakira.setStringScript(script);
-                    Object result = shakira.executeScript(script);
+                    System.out.print("Ingrese la variable: ");
+                    String variable = scanner.nextLine();
+                    Object result = shakira.executeScript(script, variable);
                     break;
                 case 3:
                     System.out.println("Saliendo del programa...");
